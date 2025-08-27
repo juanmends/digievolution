318ce4bf4b327f77112e7b51e9fc4ef0adfdadab
juanmends <juansmends@gmail.com>
2025-08-26 22:26:10 -0300

    Verificação da escrita

--- main.py
+++ main.py
@@ -15,20 +15,15 @@ def get_digimon(nome):
 #MAIN
 while True:
     nome_input = input("Digite o nome de um digimon: ")
+    nome_input = nome_input.lower().strip()
+    print(nome_input)
     digimon_escolhido = get_digimon(nome_input)
     if digimon_escolhido:
         break
     else:
         print("Digimon não encontrado")
 
-lista_prior_evolutions = digimon_escolhido["priorEvolutions"]
 lista_next_evolutions = digimon_escolhido["nextEvolutions"]
-
-print("Prior Evolutions\n")
-for digi in lista_prior_evolutions:
-    print(digi["digimon"])
-
-print("\n" + "-"*24 + "\n")
 
 print("Next Evolutions\n")
 for digi in lista_next_evolutions:
